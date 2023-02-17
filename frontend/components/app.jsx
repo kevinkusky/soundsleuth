c872c9d705b44c958b6cc8cfca2952b4556fcb56
Kevin Kusky <kevinkusky@gmail.com>
2023-02-17 09:00:37 -0500

    adds bootstrap

---
+++ frontend/components/app.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+import GreetingContainer from './greeting/greeting_container';
+import SearchContainer from './search/search_container';
+
+const App = () => (
+  <div>
+    <header>
+      <Link to="/" className="header-link">
+        <h1>SoundSleuth</h1>
+      </Link>
+      <GreetingContainer />
+    </header>
+    <Switch>
+      {/* <AuthRoute exact path="/login" component={LogInFormContainer} />
+      <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+      <ProtectedRoute exact path="/benches/new" component={BenchFormContainer} /> */}
+      <Route exact path="/" component={SearchContainer} />
+    </Switch>
+  </div>
+);
+
+export default App;
