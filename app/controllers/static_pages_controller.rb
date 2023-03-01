63f2be261bf7f78ef49ab1c9c5d9c376fbe70fe1
Kevin Kusky <kevinkusky@gmail.com>
2023-03-01 09:58:34 -0500

    clean

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,3 @@
 class StaticPagesController < ApplicationController
-  def root
-    respond_to do |format|
-      format.html {render :root}
-      format.json {remder json: {message: 'Hello Kev'}}
-    end
-  end
+  def root;end
 end
